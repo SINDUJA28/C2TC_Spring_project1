2fc6e1aae3c09ba668066c95e4d6befd3a0235e8
Sinduja28 <sindhujas902@gmail.com>
2025-11-10 06:20:03 +0530

    Delete PlacementmanagementApplication directory

--- PlacementmanagementApplication/src/main/java/com/tnsif/college/repository/CollegeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tnsif.college.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tnsif.college.entity.College;
-
-public interface CollegeRepository extends JpaRepository<College, Integer> {
-
-}
